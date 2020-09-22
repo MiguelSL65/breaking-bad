ee28ce883efcc9ebf8975ae898ed41037e475c5c
Miguel Lemos <mimiguelsl65@gmail.com>
2020-09-22 16:22:16 +0100

    add search feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from 'react'
+import axios from 'axios'
+import Header from './components/ui/Header'
+import CharacterGrid from './components/characters/CharacterGrid'
+import Search from './components/ui/Search'
+import './App.css'
 
-function App() {
+const App = () => {
+
+  const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+
+    const fetchCharacters = async () => {
+      
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      );
+
+      // console.log(result.data);
+
+      setCharacters(result.data);
+      setIsLoading(false);
+    }
+
+    fetchCharacters();
+
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="container">
+      <Header />
+      <Search getQuery={(query) => setQuery(query)} />
+      <CharacterGrid isLoading={isLoading} characters={characters} />
     </div>
   );
 }
---
+++ src/components/characters/CharacterGrid.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import CharacterItem from './CharacterItem'
+import Spinner from '../ui/Spinner'
+
+const CharacterGrid = ({ characters, isLoading }) => {
+    
+    return isLoading ? (
+        <Spinner />
+    ) : (
+
+    <section className='cards'>
+        
+        {characters.map(item => (
+            <CharacterItem key={item.char_id} item={item}></CharacterItem>
+        ))}
+        
+    </section>);
+}
+
+export default CharacterGrid
